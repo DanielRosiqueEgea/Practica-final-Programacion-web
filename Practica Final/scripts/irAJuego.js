6d197d99c3d9ca9882c3e4132abe22265816bc1d
roxoner44 <drosique6@alu.ucam.edu>
2023-03-08 18:30:08 +0100

    todo borrado

--- Practica Final/scripts/irAJuego.js
+++
@@ -1,9 +0,0 @@
-const queryString = window.location.search;
-console.log(queryString);
-const urlParams = new URLSearchParams(queryString);
-
-let nombreJuego= urlParams.get("juego");
-
-let seccionJuego  = document.getElementById("nombreJuego");
-
-seccionJuego.innerText= nombreJuego;
